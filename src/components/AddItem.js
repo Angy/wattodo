1235c4c48e41e4ea5736d42a2fb612fa67d66f5f
Angira Tripathi <Angira@MacBook-Pro-Developer.local>
2020-10-25 19:48:04 +0100

    additem component

---
+++ src/components/AddItem.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+
+function AddItem() {
+    const [item, setItem] = useState('')
+
+    return (
+        <div>
+            <h1>ting</h1>
+        </div>
+    )
+
+}
+
+export default AddItem
